3e7d3c8251931faf65b85a59ed444aedecf9e224
naty55 <na>
2021-09-23 13:35:37 +0300

    check

--- src/main/java/com/ra/resume_alternative/ResumeAlternativeApplication.java
+++ src/main/java/com/ra/resume_alternative/ResumeAlternativeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResumeAlternativeApplication {
 
 	public static void main(String[] args) {
+		// Check
 		SpringApplication.run(ResumeAlternativeApplication.class, args);
 	}
 
