7477c04069ab3e62076b9fdfda91963bbc52f444
naty55 <natymm314@gmail.com>
2021-09-23 11:33:12 +0300

    init

---
+++ src/main/java/com/ra/resume_alternative/ResumeAlternativeApplication.java
@@ -0,0 +1,13 @@
+package com.ra.resume_alternative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAlternativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAlternativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ra/resume_alternative/ResumeAlternativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ra.resume_alternative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAlternativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
