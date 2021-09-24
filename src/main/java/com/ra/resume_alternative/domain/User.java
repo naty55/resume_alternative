a08b451b6d8a791adf0880141498bb02672a9c96
naty55 <no-reply.naty55@github.com>
2021-09-24 15:52:30 +0300

    added user entity

---
+++ src/main/java/com/ra/resume_alternative/domain/User.java
@@ -0,0 +1,55 @@
+package com.ra.resume_alternative.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Override
+    public String toString() {
+        return "User [email=" + email + ", id=" + id + ", name=" + name + ", password=" + password + "]";
+    }
+
+    
+}
