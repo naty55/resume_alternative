6e1d2c9cb3bdb04f51b6ac16ef56027f276658b9
naty55 <no-reply.naty55@github.com>
2021-09-26 19:43:46 +0300

    Controller for the base website

---
+++ src/main/java/com/ra/resume_alternative/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.ra.resume_alternative.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(@RequestParam(name="name",required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+		System.out.println("WTF " + model);
+        return "test";
+    }
+    
+}
