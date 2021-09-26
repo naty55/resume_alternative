86babb1d627ee9943a9b995376ab2c7df0ccdb23
naty55 <no-reply.naty55@github.com>
2021-09-26 18:11:30 +0300

    _

--- src/main/java/com/ra/resume_alternative/ResumeAlternativeApplication.java
+++ src/main/java/com/ra/resume_alternative/ResumeAlternativeApplication.java
@@ -2,13 +2,20 @@ package com.ra.resume_alternative;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class ResumeAlternativeApplication {
 
 	public static void main(String[] args) {
-		// Check
 		SpringApplication.run(ResumeAlternativeApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String index() {
+		return "test";
+    }
 
 }
