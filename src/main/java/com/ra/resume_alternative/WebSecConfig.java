7cfe90dc9ba86d52bf28e6b51e92f8917fde0f51
naty55 <no-reply.naty55@github.com>
2021-10-29 17:38:53 +0300

    added websecconfig

---
+++ src/main/java/com/ra/resume_alternative/WebSecConfig.java
@@ -0,0 +1,39 @@
+package com.ra.resume_alternative;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/user/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+}
