2cd37b7fea126907cc71454aeb61c89b2fe534bc
naty55 <no-reply.naty55@github.com>
2021-11-04 17:24:20 +0200

    added user repository test

---
+++ src/test/java/com/ra/resume_alternative/UserRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.ra.resume_alternative;
+
+import java.text.ParseException;
+
+import com.ra.resume_alternative.domain.User;
+import com.ra.resume_alternative.repositories.Users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class UserRepositoryTest {
+    
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    Users usersRepo;
+
+    @Test
+    void testCreateUser() throws ParseException {
+        User user = new User(1, "test_user", "test_email", "12345678", "01/01/2020", "ROLE_USER");
+        
+        
+        User savedUser = usersRepo.save(user);
+        
+        User existUser = entityManager.find(user.getClass(), user.getId());
+
+        assertThat(savedUser.getEmail()).isEqualTo(existUser.getEmail());
+        assertThat(savedUser.getName()).isEqualTo(existUser.getName());
+        assertThat(savedUser.getId()).isEqualTo(existUser.getId());
+        assertThat(savedUser.getPassword()).isEqualTo(existUser.getPassword());
+    }
+    
+}
