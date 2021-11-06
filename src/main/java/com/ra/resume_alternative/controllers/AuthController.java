aef32c6cdd66f9a2404cd35181da007d4695a5ea
naty55 <no-reply.naty55@github.com>
2021-11-06 22:17:35 +0200

    added usage in securityService and removed some unused objects

--- src/main/java/com/ra/resume_alternative/controllers/AuthController.java
+++ src/main/java/com/ra/resume_alternative/controllers/AuthController.java
@@ -1,28 +1,19 @@
 package com.ra.resume_alternative.controllers;
 
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Date;
 
+import com.ra.resume_alternative.security.SecurityService;
 import com.ra.resume_alternative.domain.User;
 import com.ra.resume_alternative.repositories.Users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -33,43 +24,35 @@ public class AuthController {
     PasswordEncoder passwordEncoder;
 
     @Autowired
-    AuthenticationManagerBuilder auth;
-
-    @Autowired
     Users usersRepo;
 
-
+    @Autowired
+    SecurityService securityService;
     
     @GetMapping("/login")
-    String login(Model model, UsernamePasswordAuthenticationToken principal) {
-        if (principal != null && principal.isAuthenticated()) {
-            return "redirect:/";
-        }
-        return "login";
+    String login(Model model, Authentication auth) {
+        return securityService.isAuthenticatedAndView(auth, "/", "login");
     }
-    @RequestMapping("/logout")
-    RedirectView logout() {
-        return new RedirectView("/", false, true, false);
-    }
+    // @RequestMapping("/logout")
+    // RedirectView logout(Authentication auth) {
+    //     return new RedirectView("/", false, true, false);
+    // }
 
     @GetMapping("/signup")
-    String signup(Model model) {
+    String signup(Model model, Authentication auth) {
         model.addAttribute("user", new User());
-        return "signup";
+        return securityService.isAuthenticatedAndView(auth, "/", "signup");
     }
 
     @PostMapping("/signup")
-    RedirectView signup(Model model, User user) {
+    String signup(Model model, User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setCreated(Calendar.getInstance().getTime());
         user.setRoles("ROLE_USER");
         usersRepo.save(user);
-
-        Authentication auth = new UsernamePasswordAuthenticationToken(user.getName(), user.getPassword(), Arrays.asList(new SimpleGrantedAuthority(user.getRoles())));
-        SecurityContextHolder.getContext().setAuthentication(auth);
-        model.addAttribute("name", auth.getName());
-        model.addAttribute("isLoogedIn", auth.isAuthenticated());
-        return new RedirectView("/", false, true, false);
+        Authentication auth = securityService.autoLogin(user);
+        securityService.isAuthenticatedAndModel(auth, model);
+        return "redirect:/";
     }
 
 }
