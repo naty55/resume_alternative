14d068fcaad0159371c427ec393d8331a9b8c9a5
naty55 <no-reply.naty55@github.com>
2021-11-06 22:34:11 +0200

    adding custom Authentication provider

--- src/main/java/com/ra/resume_alternative/SecurityConfig.java
+++ src/main/java/com/ra/resume_alternative/SecurityConfig.java
@@ -2,15 +2,12 @@ package com.ra.resume_alternative;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 
@@ -21,9 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsService userDetailsService;
 
+	@Autowired
+	MysqlAuthProvider authProvider;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+		auth.authenticationProvider(authProvider);
     }
 
     @Override
@@ -31,17 +31,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 			.antMatchers("/*", "/auth/**").permitAll()
-			.antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/user/**", "/build/**").hasAnyRole("USER", "ADMIN")
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
 			.loginPage("/auth/login")
-			.defaultSuccessUrl("/")
-			.and().logout().logoutSuccessUrl("/");
+			.and()
+			.logout()
+			.logoutUrl("/auth/logout")
+			.logoutSuccessUrl("/")
+			.deleteCookies("JSESSIONID");
 	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	
 } 
