91dfbd65193b972e44bc1908e599e40f95160802
naty55 <no-reply.naty55@github.com>
2021-11-06 21:58:01 +0200

    removed securityContext because it's done automotically

--- src/main/java/com/ra/resume_alternative/MysqlAuthProvider.java
+++ src/main/java/com/ra/resume_alternative/MysqlAuthProvider.java
@@ -1,13 +1,10 @@
 package com.ra.resume_alternative;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -25,11 +22,14 @@ public class MysqlAuthProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         UserDetails userDetails = userDetailsService.loadUserByUsername(authentication.getName());
-        if (passwordEncoder.matches(authentication.getCredentials().toString(), userDetails.getPassword()) && 
-        userDetails.getUsername().equals(authentication.getName())) {
-            Authentication a =  new UsernamePasswordAuthenticationToken(authentication.getName(), authentication.getCredentials().toString(), userDetails.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(a);
-            return a;
+        if (
+            passwordEncoder.matches(authentication.getCredentials().toString(), userDetails.getPassword()) && 
+            userDetails.getUsername().equals(authentication.getName())
+            ) {
+            return new UsernamePasswordAuthenticationToken(
+                authentication.getName(),
+                authentication.getCredentials().toString(), 
+                userDetails.getAuthorities());
         }
         return null;
     }
