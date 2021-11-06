ae9ac64926d1ad56957afd52aa697c45a009b84f
naty55 <no-reply.naty55@github.com>
2021-11-06 18:41:12 +0200

    cleaning, and handling security stuff

--- src/main/java/com/ra/resume_alternative/controllers/HomeController.java
+++ src/main/java/com/ra/resume_alternative/controllers/HomeController.java
@@ -1,34 +1,39 @@
 package com.ra.resume_alternative.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.List;
 
+import com.ra.resume_alternative.domain.User;
+import com.ra.resume_alternative.repositories.Users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        Map map = new HashMap();
-        map.put("name", name);
-        model.addAttribute("map", map);
+    @Autowired
+    Users repo;
+
+    @RequestMapping("/")
+    public String index(Model model, Authentication auth) {
+        if (auth != null) {
+            model.addAttribute("name", auth.getName());
+            model.addAttribute("isLoggedIn", true);   
+        }
+
+        model.addAttribute("users", repo.findAll());
         return "index";
     }
 
-    @GetMapping(value = {"/build/{user}", "/build"})
+    @GetMapping(value="/users", produces=MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String build(@PathVariable Optional<String> user) {
-        if (user.isPresent()) {
-            return "Hello " + user.get();
-        }
-        return "Hello World";
+    public List<User> users() {
+        return repo.findAll();
     }
-    
 }
