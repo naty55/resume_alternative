005f3961908248c14a1f5874c7c505e2b3f69889
naty55 <no-reply.naty55@github.com>
2021-11-06 21:54:11 +0200

    added method to find by name

--- src/main/java/com/ra/resume_alternative/repositories/Users.java
+++ src/main/java/com/ra/resume_alternative/repositories/Users.java
@@ -1,6 +1,10 @@
 package com.ra.resume_alternative.repositories;
+import java.util.Optional;
+
 import com.ra.resume_alternative.domain.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Users extends JpaRepository<User, Long>{}
+public interface Users extends JpaRepository<User, Long>{
+    Optional<User> findByName(String username);
+}
