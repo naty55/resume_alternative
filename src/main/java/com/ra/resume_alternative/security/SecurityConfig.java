51f02594536db5d92a5f2975061fa8f1ae382602
naty55 <no-reply.naty55@github.com>
2021-12-17 16:36:57 +0200

    removed unused imports

--- src/main/java/com/ra/resume_alternative/security/SecurityConfig.java
+++ src/main/java/com/ra/resume_alternative/security/SecurityConfig.java
@@ -4,9 +4,7 @@ package com.ra.resume_alternative.security;
 import com.ra.resume_alternative.auth.MysqlAuthProvider;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.antMatchers("/**", "/auth/**").permitAll()
+			.antMatchers("/*","/css/*", "/auth/**").permitAll()
 			.antMatchers("/user/**", "/build/**").hasAnyRole("USER", "ADMIN")
 			.anyRequest().authenticated()
 			.and()
@@ -48,11 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.logoutSuccessUrl("/")
 			.deleteCookies("JSESSIONID");
 	}
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		 return super.authenticationManagerBean();
-	}
+	
 
 	
 } 
