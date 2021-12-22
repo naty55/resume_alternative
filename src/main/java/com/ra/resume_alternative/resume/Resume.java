c76aac3463536c3178d9bd6a110d85caea82be23
naty55 <no-reply.naty55@github.com>
2021-12-22 17:10:57 +0200

    complete defining resume relations

--- src/main/java/com/ra/resume_alternative/resume/Resume.java
+++ src/main/java/com/ra/resume_alternative/resume/Resume.java
@@ -1,7 +1,9 @@
 package com.ra.resume_alternative.resume;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,7 +15,6 @@ import javax.persistence.OneToMany;
 import com.ra.resume_alternative.user.User;
 
 
-
 @Entity(name = "resume")
 public class Resume {
 
@@ -22,16 +23,74 @@ public class Resume {
     Long resumeId;
 
     @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id")
     User user;
 
     String title;
 
-    @OneToMany(mappedBy = "resume", orphanRemoval = true)
-    Set<ResumeBlock> blocks;
+    @OneToMany(mappedBy = "resume", orphanRemoval = true, cascade = CascadeType.ALL)
+    Set<ResumeBlock> blocks = new HashSet<>();
 
-    @OneToMany(orphanRemoval = true)
-    @JoinColumn(name = "resume_id", nullable=false)
-    Set<ResumeSkill> skills;
+    @OneToMany(mappedBy ="resume", orphanRemoval = true, cascade = CascadeType.ALL)
+    Set<ResumeSkill> skills = new HashSet<>();
+
+    public Resume() {
+
+    }
+    public Resume(User user,String title, Set<ResumeBlock> blocks, Set<ResumeSkill> skills) {
+        this.user = user;
+        this.title = title;
+        setBlocks(blocks);
+        setSkills(skills);
+    }
+    public Long getResumeId() {
+        return resumeId;
+    }
+    public void setResumeId(Long resumeId) {
+        this.resumeId = resumeId;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Set<ResumeBlock> getBlocks() {
+        return blocks;
+    }
+    public void addBlock(ResumeBlock block) {
+        blocks.add(block);
+        block.setResume(this);
+    }
+    public void removeBlock(ResumeBlock block) {
+        blocks.remove(block);
+        block.setResume(null);
+    }
+    public void setBlocks(Set<ResumeBlock> blocks) {
+        blocks.forEach(b -> this.addBlock(b));
+    }
+    public Set<ResumeSkill> getSkills() {
+        return skills;
+    }
+    public void setSkills(Set<ResumeSkill> skills) {
+        skills.forEach(s -> this.addSkill(s));
+    }
+    public void addSkill(ResumeSkill skill) {
+        skills.add(skill);
+        skill.setResume(this);
+    }
+    public void removeSkill(ResumeSkill skill) {
+        skills.remove(skill);
+        skill.setResume(null);
+    }
+
+    
+
     
 }
