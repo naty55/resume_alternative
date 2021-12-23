ae08556ccdc1b19a2658de08c55e8741a0bd7668
naty55 <no-reply.naty55@github.com>
2021-12-23 23:09:53 +0200

    added Detail type entity

---
+++ src/main/java/com/ra/resume_alternative/resume/DetailType.java
@@ -0,0 +1,12 @@
+package com.ra.resume_alternative.resume;
+
+public enum DetailType {
+    Phone,
+    Email,
+    LinkedIn,
+    Twitter,
+    Github,
+    Instegram,
+    Facebook,
+    SocialMedia
+}
