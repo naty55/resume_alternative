9f74c152853c2c084164c3668357b7dd5394c120
naty55 <64107292+naty55@users.noreply.github.com>
2021-12-30 22:30:52 +0200

    allow static js requests

--- src/main/java/com/ra/resume_alternative/security/SecurityConfig.java
+++ src/main/java/com/ra/resume_alternative/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.antMatchers("/*","/css/*", "/auth/**").permitAll()
+			.antMatchers("/*", "/js/*","/css/*", "/auth/**").permitAll()
 			.antMatchers("/build/**", "/user/**").hasAnyRole("USER", "ADMIN")
 			.anyRequest().authenticated()
 			.and()
