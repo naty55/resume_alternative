e3521ddc7ed4a10316b4ef73e88a58d68d44a8f8
naty55 <64107292+naty55@users.noreply.github.com>
2021-12-30 22:30:30 +0200

    adding methods to resume service

--- src/main/java/com/ra/resume_alternative/resume/ResumeRepository.java
+++ src/main/java/com/ra/resume_alternative/resume/ResumeRepository.java
@@ -1,12 +1,14 @@
 package com.ra.resume_alternative.resume;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.ra.resume_alternative.resume.entity.Resume;
 import com.ra.resume_alternative.user.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
     List<Resume> findByUser(User user);
-
+    Optional<Resume> findByResumeId(Long resumeId);
 }
--- src/main/java/com/ra/resume_alternative/resume/ResumeService.java
+++ src/main/java/com/ra/resume_alternative/resume/ResumeService.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.ra.resume_alternative.resume.entity.Resume;
 import com.ra.resume_alternative.user.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,19 @@ public class ResumeService {
             rv.add(map);
         }
         return rv;
+    }
+
+    public Resume getResumeById(Long id) {
+        return resumeRepository.findById(id).get();
+    }
+
+    public boolean isResumeMatchUser(User user, Resume resume) {
+        return resume.getUser().equals(user);
+    }
+
+    public Resume create(User user) {
+        Resume resume = new Resume();
+        resume.setUser(user);
+        return resumeRepository.save(resume);
     }
 }
