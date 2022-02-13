7ab611476492712be21156185c303927adb03d7c
naty55 <64107292+naty55@users.noreply.github.com>
2022-02-13 16:46:01 +0200

    adding password encoder bean

--- src/main/java/com/ra/resume_alternative/security/SecurityConfig.java
+++ src/main/java/com/ra/resume_alternative/security/SecurityConfig.java
@@ -4,12 +4,15 @@ package com.ra.resume_alternative.security;
 import com.ra.resume_alternative.auth.MysqlAuthProvider;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 
@@ -45,6 +48,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.logoutUrl("/auth/logout")
 			.logoutSuccessUrl("/")
 			.deleteCookies("JSESSIONID");
+	}
+
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 
