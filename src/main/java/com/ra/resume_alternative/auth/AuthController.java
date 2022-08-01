b31ae47996a3e646ea97482d0f1ea6d098e0152b
naty55 <64107292+naty55@users.noreply.github.com>
2022-08-01 19:20:06 +0300

    Polishing

--- src/main/java/com/ra/resume_alternative/auth/AuthController.java
+++ src/main/java/com/ra/resume_alternative/auth/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("auth/")
+@RequestMapping("auth")
 public class AuthController {
 
 
