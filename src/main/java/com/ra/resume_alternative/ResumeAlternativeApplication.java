70015bd3410376bf510f0584d2fa062b87b3943f
naty55 <64107292+naty55@users.noreply.github.com>
2022-10-12 21:40:38 +0300

    nothing

--- src/main/java/com/ra/resume_alternative/ResumeAlternativeApplication.java
+++ src/main/java/com/ra/resume_alternative/ResumeAlternativeApplication.java
@@ -1,24 +1,21 @@
 package com.ra.resume_alternative;
 
-import java.util.Optional;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-import com.ra.resume_alternative.resume.entity.Resume;
-import com.ra.resume_alternative.resume.entity.ResumeBlock;
 import com.ra.resume_alternative.resume.entity.ResumeSkill;
 import com.ra.resume_alternative.resume.entity.SkillLevel;
 import com.ra.resume_alternative.resume.entity.SkillType;
 import com.ra.resume_alternative.resume.repository.ResumeRepository;
+import com.ra.resume_alternative.resume.repository.SkillRepository;
 import com.ra.resume_alternative.resume.service.BlockService;
 import com.ra.resume_alternative.resume.service.ResumeService;
 import com.ra.resume_alternative.resume.service.SkillService;
 import com.ra.resume_alternative.user.User;
 import com.ra.resume_alternative.user.UserRepository;
 import com.ra.resume_alternative.user.UserService;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 
@@ -31,54 +28,26 @@ public class ResumeAlternativeApplication {
 	}
 
 	@Bean
-	InitializingBean fillDataBase(UserRepository repo,ResumeService resumeService ,ResumeRepository resumeRepo, SkillService skillService, UserService userService, BlockService blockService) {
+	InitializingBean fillDataBase(UserRepository repo,ResumeService resumeService ,ResumeRepository resumeRepo, SkillService skillService,SkillRepository skillRepository, UserService userService, BlockService blockService) {
 		return () -> {
-			// resumeRepo.deleteAll();
-			repo.deleteAll();
-			User user = new User();
-			user.setFirstName("Naty");
-			user.setLastName("Mina");
-			user.setEmail("naty@gmail.com");
-			user.setPassword("12345678");
-			user.setUsername("naty55");
-			user.setVerfiedEmail(true);
-			user = userService.createUser(user);
+			try {
+			// User user = new User();
+			// user.setFirstName("Naty");
+			// user.setLastName("Mina");
+			// user.setEmail("naty@gmail.com");
+			// user.setPassword("12345678");
+			// user.setUsername("naty55");
+			// user.setVerfiedEmail(true);
+			User user = repo.getById(1L);
 			ResumeSkill skill1 = skillService.addSkill(user.getUserId(), "English", SkillType.Language, SkillLevel.D);
+			skillService.addSkill(skill1);
 			ResumeSkill skill2 = skillService.addSkill(user.getUserId(), "Python", SkillType.Technology, SkillLevel.A);
 			ResumeSkill skill3 = skillService.addSkill(user.getUserId(), "Java", SkillType.Technology, SkillLevel.A);
-			Resume r1 = resumeService.addResume(user, Optional.ofNullable("Java software Developer"), Optional.ofNullable(null));
-			Resume r2 = resumeService.addResume(user, Optional.ofNullable(null), Optional.ofNullable(null));
-			r1.addSkill(skill1);
-			r1.addSkill(skill2);
-			r1.addSkill(skill3);
-			
-			r2.addSkill(skill1);
-			r2.addSkill(skill2);
-
-			ResumeBlock block1 = blockService.addBlock(user.getUserId(), r2.getResumeId(), 1, "Profile");
-			ResumeBlock block2 = blockService.addBlock(user.getUserId(), r2.getResumeId(), 1, "Employment History");
-
-			user.addResume(r1);
-			user.addResume(r2);
-			repo.save(user);
-
-			
-
-			resumeService.deleteResume(user.getUserId(), r2.getResumeId());
-
-			System.out.println(resumeService.getResumeByIdAndUserId(user.getUserId(), r1.getResumeId()));
+			skillService.deleteSkill(user.getUserId(), skill1.getSkillId());
+			} catch(Exception e) {
+				System.out.println("Fill DB didn't finish");
+			}
 		};
 	}
-
-	@Bean
-	InitializingBean printAllBeans(org.springframework.context.ApplicationContext ctx) {
-		return () -> {
-			// System.out.println("Beans that had been initialized on stratup\n" +
-			// "===================================\n");
-			// Arrays.stream(ctx.getBeanDefinitionNames()).sorted().forEach(System.out::println);
-		};
-	}
-
-	
 
 }
