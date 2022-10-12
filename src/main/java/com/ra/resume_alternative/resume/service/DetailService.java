99f84a6a20ddd9d685c0ccc99d637891edc6b973
naty55 <64107292+naty55@users.noreply.github.com>
2022-10-12 16:42:44 +0300

    Added updateOrCreate option

--- src/main/java/com/ra/resume_alternative/resume/service/DetailService.java
+++ src/main/java/com/ra/resume_alternative/resume/service/DetailService.java
@@ -1,10 +1,12 @@
 package com.ra.resume_alternative.resume.service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import com.ra.resume_alternative.resume.entity.DetailType;
 import com.ra.resume_alternative.resume.entity.ResumeDetail;
-import com.ra.resume_alternative.resume.error.RequestedEntityNotFoundException;
+import com.ra.resume_alternative.error.RequestedEntityNotFoundException;
 import com.ra.resume_alternative.resume.repository.DetailRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,15 @@ public class DetailService {
     public ResumeDetail addDetail(Long userId, String value, DetailType detailType) {
         ResumeDetail detail = new ResumeDetail(value, detailType);
         detail.setUserId(userId);
-        detail = detailRepository.save(detail);
-        return detail;
+        return addDetail(detail);
+    }
+
+    public ResumeDetail addDetail(ResumeDetail detail) {
+        Optional<ResumeDetail> det = detailRepository.findByDetailTypeAndValueAndUserId(detail.getDetailType(), detail.getValue(), detail.getUserId());
+        if(det.isPresent()) {
+            return det.get();
+        }
+        return detailRepository.save(detail);
     }
     
     public ResumeDetail updateDetail(Long userId, Long detailId, Optional<String> value) throws RequestedEntityNotFoundException {
@@ -33,13 +42,31 @@ public class DetailService {
         value.ifPresent(v -> detail.setValue(v));
         return detailRepository.save(detail);
     }
+
+    public Set<ResumeDetail> updateOrCreate(Set<ResumeDetail> details, Long userId) {
+        if (details == null) {
+            return null;
+        }
+        Set<ResumeDetail> persistedDetails = new HashSet<>();
+        for (ResumeDetail detail : details) {
+            detail.setUserId(userId);
+            ResumeDetail r = null;
+            if(detail.getDetailId() != null) {
+                r = updateDetail(userId, detail.getDetailId(), Optional.of(detail.getValue()));
+            } else {
+                r = addDetail(detail);
+            }
+            persistedDetails.add(r);
+        }
+        return persistedDetails;
+    }
     
     @Transactional
     public boolean deleteDetail(Long userId, Long detailId) {
         ResumeDetail detail = detailRepository.findById(detailId).orElseThrow(RequestedEntityNotFoundException::new);
         if(detail.getUserId().equals(userId)) {
             detailRepository.deleteAllDetailResumeConnections(detailId);
-            detailRepository.deleteBySkillIdAndUserId(detailId, userId);   
+            detailRepository.deleteByDetailIdAndUserId(detailId, userId);   
         } else {
             throw new RequestedEntityNotFoundException();
         }
